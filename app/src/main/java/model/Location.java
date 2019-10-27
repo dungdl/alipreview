a263758d889bc5dd2543f1fd8631033fdbbb0cfd
dungdl28d11 <phongnt98@gmail.com>
2019-10-27 16:18:38 +0700

    GreenDAO generated classes

--- app/src/main/java/model/Location.java
+++ app/src/main/java/model/Location.java
@@ -1,14 +1,27 @@
 package model;
 
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Generated;
+
+@Entity
 public class Location {
+
+    @Id
     private String id;
+
     private double lat;
     private double lon;
 
+    @Generated(hash = 711763354)
     public Location(String id, double lat, double lon) {
         this.id = id;
         this.lat = lat;
         this.lon = lon;
+    }
+
+    @Generated(hash = 375979639)
+    public Location() {
     }
 
     public String getId() {
@@ -29,5 +42,9 @@ public class Location {
 
     public void setLon(double lon) {
         this.lon = lon;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 }
